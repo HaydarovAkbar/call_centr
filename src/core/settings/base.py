d9e72b2b3a3d324c27fa20dabf96035fdf41b385
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2024-07-31 16:55:49 +0500

    dsf

--- src/core/settings/base.py
+++ src/core/settings/base.py
@@ -152,7 +152,7 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
-    'user_model': 'account.User',
+    # 'user_model': 'account.User',
 }
 # AXES settings
 
